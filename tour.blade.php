6db3d8d4e75c0627c53e965a9477b311b6b03bbf
kaweng pam <kawengpam56@gmail.com>
2022-04-26 00:56:45 +0100

    Add files via upload

---
+++ tour.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tour Safari Gallery</title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css">
+     <link rel="stylesheet" href="/main/style.css">
+
+     <!-- <style>
+     </style> -->
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    <div class="container">
+            <div class="navbar-header">
+            <a class="navbar-brand" href="a"><span class="glyphicon glyphicon-send" aria-hidden="true"></span> Tour</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+  <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Tap for more">
+           </div>
+           <button type="submit" class="btn btn-default">Search</button>
+</form>
+           
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Sign up</a></li>
+                <li><a href="#">Login</a></li>
+             </ul>
+   </div>
+</nav>
+
+<div class ="container">
+        <div class="jumbotron">
+
+                <h1><span class="glyphicon glyphicon-plane" aria-hidden="true"></span>Tour Safari Gallery</h1>
+                <p>Bringing you so many beautiful sites you are still yet to discover.</p>
+         </div>
+
+       
+      <div class="row">
+                <div class="col-sm-4 col-md-4">
+                 <div class="thumbnail">
+                        <img  src="/images/josRiyom.jpg">  
+                     <div class="caption">
+                         <p> Riyom Rock formation found along...</p>
+                         <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                     </div>
+                 </div>
+             </div>
+         
+
+       
+            <div class="col-sm-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="/images/Pandam.jpg"> 
+                            <div class="caption">
+                                <p>Located in Pandam in Qua'an Pan...</p>
+                                <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                            </div>
+                        </div>
+                </div> 
+
+           <div class="col-sm-4 col-md-4">
+                <div class="thumbnail">
+                      <img src="/images/wasse.jpg">  
+                    <div class="caption">
+                         <p>Wasse Rock is Located in Wasse Town...</p>
+                         <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                     </div>
+              </div>
+          </div>
+          
+ </div>
+
+        <div class="row">
+                <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/national.jpg">  
+                    <!-- <div class="caption"> -->
+                    <p>National Museum and Monuments in Jos Plateau State.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div> 
+             <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/Kurra.jpg">  
+                    <p>Located in plateau state, Riyom Local Government Area.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+
+                </div>
+            </div>
+             <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/naraguta.jpg">  
+                    <p>Naraguta Leather works found in Naraguta, Plateau state.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                
+                </div>
+              </div>
+        </div>
+
+  <div class="row">
+             <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/HELENA.jpg">  
+                    <p>Helena Farm known as Rose Merchant located in Jos Plateau state.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Readmore</a></p>
+
+                </div>
+            </div> 
+            
+             <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/shere.jpg">  
+                    <p>Shere Hills is one of the highest peaks for mountain climbers and is found in Jos Plateau State. </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Readmore</a></p>
+              
+                </div>
+            </div> 
+    
+             <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/volcanic.jpg">  
+                    <p> Kurang volcanic Mountain which supplies popular spring water(SWAN)88km away from Jos.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Readmore</a></p>
+
+                </div>
+            </div>
+
+<div class="row">
+             <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/zoo.jpg">  
+                    <p>Wild LIfe Park located in Federal Lowcost Jos Plateau State.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+
+
+                </div>
+            </div>
+        
+             <div class="col-1g-4 col-xs-4">
+                <div class="thumbnail">
+                    <img src="/images/rayresorts.jpg"> 
+                    <p>Ryfield Resorts located in Rayfield Jos Plateau State.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+
+                </div>
+            </div> 
+            
+             <div class="col-1g-4 col-xs-4">
+                 <div class="thumbnail">
+                    <img src="/images/Assop.jpg">  
+                    <p>Assop Waterfalls located in Jos-Kagoro Road.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+
+                 </div>
+            </div> 
+         
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4 col-xs-6.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+
+
+
+
+</body>
+</html>
